64f823fa07abf67dc6fbfaf204ef64638fbc39cb
Ayyash <abdul_ayyash@yahoo.com>
2020-10-30 22:16:38 +0200

    icons

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -8,4 +8,9 @@ exports.rawless = assets.rawless; // create minisite src/css/bo.css
 exports.buildcss = assets.buildcss; // build minisite public/css/bo.min.css 
 exports.default = assets.watch; // watch minisite to create src/css/bo.css
 
+// create icons from mockup dummy files
 exports.iconset = icons.iconset;
+// to prepare icons from remote folder
+exports.prepicons = icons.prepicons;
+// to do both
+exports.createicons = icons.createicons;
