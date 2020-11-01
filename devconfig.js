7335dd1dd54eb810316debe58e2e96862117307c
Ayyash <abdul_ayyash@yahoo.com>
2020-11-01 17:34:21 +0200

    modal

--- devconfig.js
+++ devconfig.js
@@ -4,11 +4,32 @@ del.sync("./_site");
 
 module.exports = function (eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy({"src/assets": "assets"});
+    eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
 
-    eleventyConfig.addShortcode("mirror", function() {
+    eleventyConfig.addShortcode("mirror", function () {
         return `<span class="label label-blue smaller">mirored</span>`;
-      });
+    });
+
+
+    eleventyConfig.addPairedShortcode("modal", function (content, title, trigger, footer) {
+        return `
+        <div class="dr-modal-overlay modal-overlay" data-trigger="${trigger}">
+            <div class="modal dr-window" role="dialog" aria-labelledby="modaltitle" tabindex="-1">
+                <div class="modal-dialog">
+                    <div class="modal-content dr-content">
+                        <div class="modal-header">
+                            <h6 class="f6 modal-title dr-title">${title}</h6>
+                            <button type="button" class="modal-close dr-close" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ${content}
+                        </div>
+                        ${ footer ? `<div class="modal-footer">${footer}</div>` : ``}                        
+                    </div>
+                </div>
+            </div>
+        </div>`;
+    });
 
     return {
         passthroughFileCopy: true,
---
+++ src/assets/js/shut.js
@@ -0,0 +1,77 @@
+
+
+class Modal {
+
+    config = {
+        modalSelector: '.dr-modal-overlay',
+        closeSelector: '.dr-close',
+        titleSelector: '.dr-title',
+        modalContentSelector: '.dr-content',
+        bodyCss: 'mdl-open'
+    };
+
+
+    constructor(element) {
+        this.modalElement = element;
+        this.trigger = element.getAttribute('data-trigger');
+
+        this.modalElement.addEventListener('click', event => {
+            const target = event.target;
+
+            if (!target.closest(this.config.modalContentSelector)) {
+                this.hide();
+            }
+            // if target is close, hide
+            if (target.matches(this.config.closeSelector)) {
+                this.hide();
+            }
+        });
+
+        window.document.addEventListener('click', event => {
+            const target = event.target;
+
+            if (this.trigger) {
+                if (target.matches(this.trigger) || target.closest(this.trigger)) {
+                    // itself or its parent
+                    // const _t = target.matches(this.trigger) ? target : target.closest(this.trigger);
+                    this.show();
+                }
+            }
+        });
+    }
+    show = () => {
+
+         this.modalElement.style.display = 'block';
+         document.body.classList.add(this.config.bodyCss);
+    };
+
+    hide = () => {
+        this.modalElement.style.display = 'none';
+        document.body.classList.remove(this.config.bodyCss);
+        
+    };
+
+
+}
+
+function setVh() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    // set --vh
+    setVh();
+
+    window.addEventListener('resize', setVh);
+
+    // find dialogs and attach behaviors
+    const modals = document.querySelectorAll('.dr-modal-overlay');
+
+    modals.forEach(n => {
+
+        new Modal(n);
+
+    });
+
+});
